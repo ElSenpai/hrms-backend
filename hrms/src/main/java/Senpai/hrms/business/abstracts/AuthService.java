815accf5f4ea620e621a142e5b0b6ca4b38d8344
El Senpai <El Senpai@EL-SENPAI>
2021-05-10 00:03:28 +0300

    confirmpass

--- hrms/src/main/java/Senpai/hrms/business/abstracts/AuthService.java
+++ hrms/src/main/java/Senpai/hrms/business/abstracts/AuthService.java
@@ -1,6 +1,9 @@
 package Senpai.hrms.business.abstracts;
 
+import Senpai.hrms.entities.concretes.JobHunter;
+
 public interface AuthService {
 
-	
+	void register (JobHunter jobHunt,String confirmPass);
+	void login (JobHunter jobHunt);
 }
--- hrms/src/main/java/Senpai/hrms/business/concretes/AuthManager.java
+++ hrms/src/main/java/Senpai/hrms/business/concretes/AuthManager.java
@@ -1,7 +1,39 @@
 package Senpai.hrms.business.concretes;
 
-import Senpai.hrms.business.abstracts.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import Senpai.hrms.business.abstracts.AuthService;
+import Senpai.hrms.business.abstracts.JobHunterService;
+import Senpai.hrms.entities.concretes.JobHunter;
+@Service
 public class AuthManager implements AuthService {
+	
+	private JobHunterService jobHunterService;
+	
+    @Autowired
+	public AuthManager(JobHunterService jobHunterService) {
+		super();
+		this.jobHunterService = jobHunterService;
+	}
+
+	@Override
+	public void register(JobHunter jobHunt, String confirmPass) {
+		
+		if(jobHunt.getEmail()==confirmPass) {
+			this.jobHunterService.add(jobHunt);
+		} else {
+			System.out.println("password did not match!");
+		}
+		
+	}
+
+	@Override
+	public void login(JobHunter jobHunt) {
+		//after dto
+		
+	}
+	
+	
 
 }
