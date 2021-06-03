4e2b96a789cfbb3a208debd74ccd249f3d7db590
El Senpai <El Senpai@EL-SENPAI>
2021-06-03 20:29:17 +0300

    cv dto

---
+++ hrms/src/main/java/Senpai/hrms/entities/dto/CvDto.java
@@ -0,0 +1,27 @@
+package Senpai.hrms.entities.dto;
+
+import java.util.List;
+
+import Senpai.hrms.entities.concretes.Ability;
+import Senpai.hrms.entities.concretes.CoverLetter;
+import Senpai.hrms.entities.concretes.Education;
+import Senpai.hrms.entities.concretes.Experience;
+import Senpai.hrms.entities.concretes.Image;
+import Senpai.hrms.entities.concretes.JobHunter;
+import Senpai.hrms.entities.concretes.Language;
+import Senpai.hrms.entities.concretes.Link;
+
+public class CvDto {
+
+	private JobHunter jobhunter;
+	private Image image;
+	private List<CoverLetter> coverletters;
+	private List<Education> educations;
+	private List<Experience> experiences;
+	private List<Ability>	abilities;
+	private List<Language> languages;
+	private List<Link> links;
+	
+	
+	
+}
